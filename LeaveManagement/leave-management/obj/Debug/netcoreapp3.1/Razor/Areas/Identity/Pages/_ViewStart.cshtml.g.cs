c65a5c8d96d890ce3790a83ff78343a303d49c41
Bussa, Jayasimha (CORP) <Jayasimha.Bussa@adp.com>
2020-05-10 02:02:16 +0530

    Added Identitiy PageModel for Authentication

--- LeaveManagement/leave-management/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ LeaveManagement/leave-management/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Jayasimha Files\Learning\May-7 New Learnings\DotNetCoreByTrevoirWilliams_GitClone\LeaveManagement\leave-management\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Jayasimha Files\Learning\May-7 New Learnings\DotNetCoreByTrevoirWilliams_GitClone\LeaveManagement\leave-management\Areas\Identity\Pages\_ViewImports.cshtml"
+using leave_management.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Jayasimha Files\Learning\May-7 New Learnings\DotNetCoreByTrevoirWilliams_GitClone\LeaveManagement\leave-management\Areas\Identity\Pages\_ViewImports.cshtml"
+using leave_management.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"0ef7af54b6a7e0d0132ba578e8287c41deb44f52", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
